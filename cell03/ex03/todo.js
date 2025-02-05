09ece0f51b77e29d8a90d84d21789df75267848e
patdanai767 <pattproc@gmail.com>
2025-02-05 18:49:56 +0700

    update ex03

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,42 +1,31 @@
-const textList = document.getElementById("todolist");
+// const textList = document.getElementById("todolist");
 const list = document.getElementById("ft_list");
 
 function addList() {
-  if (textList.value !== "") {
+  const val = prompt("What is your name?")
+  if (val !== "" & val !== null) {
     var li = document.createElement("p");
-    li.innerHTML = textList.value;
+    li.innerHTML = val;
     list.appendChild(li);
     var x = document.createElement("span");
     x.innerHTML = "\u00d7";
     li.appendChild(x);
     console.log(list);
     document.cookie = `data=${list.outerHTML}`;
-    textList.value = null;
   }
 }
-
-function getCookies(name) {
-  let cookies = document.cookie.split('; ');
-  for (let cookie of cookies) {
-      let [key, value] = cookie.split('=');
-      if (key === name) return decodeURIComponent(value);
-  }
-  return "";
-}
-
-console.log(getCookies("data"))
 
 list.addEventListener("click", (e) => {
   if (e.target.tagName === "SPAN") {
     const checked = confirm(`Are you sure to delete?`);
     if (checked) {
       e.target.parentElement.remove();
-      document.cookie = "";
+      document.cookie = `data=${list.outerHTML}`
     }
   }
 });
 
 function showTask() {
-  list.innerHTML = document.cookie;
+  list.innerHTML = document.cookie.substring(5);
 }
 showTask();
