6816cc9474b111f4864fee216b3d9447e148fe82
patdanai767 <pattproc@gmail.com>
2025-02-05 17:45:10 +0700

    update calc

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -2,15 +2,29 @@ const textList = document.getElementById("todolist");
 const list = document.getElementById("ft_list");
 
 function addList() {
-  var li = document.createElement("p");
-  li.innerHTML = textList.value;
-  list.appendChild(li);
-  var x = document.createElement("span");
-  x.innerHTML = "\u00d7";
-  li.appendChild(x);
-  document.cookie = `data=${list}`;
-  textList.value = null;
+  if (textList.value !== "") {
+    var li = document.createElement("p");
+    li.innerHTML = textList.value;
+    list.appendChild(li);
+    var x = document.createElement("span");
+    x.innerHTML = "\u00d7";
+    li.appendChild(x);
+    console.log(list);
+    document.cookie = `data=${list.outerHTML}`;
+    textList.value = null;
+  }
 }
+
+function getCookies(name) {
+  let cookies = document.cookie.split('; ');
+  for (let cookie of cookies) {
+      let [key, value] = cookie.split('=');
+      if (key === name) return decodeURIComponent(value);
+  }
+  return "";
+}
+
+console.log(getCookies("data"))
 
 list.addEventListener("click", (e) => {
   if (e.target.tagName === "SPAN") {
