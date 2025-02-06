038b971f451303c7315f86297710a340b836305f
patdanai767 <pattproc@gmail.com>
2025-02-06 15:18:15 +0700

    finish 100%

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,24 +1,23 @@
 $(document).ready(function () {
   var cookies = document.cookie;
-  var list = document.cookie.substring(5).split(",");
   $(".btn-add").on("click", function () {
     const val = encodeURIComponent(prompt());
-    if ((val !== null) & (val !== "")) {
+    if ((val !== "null") & (val !== "") & (val !== null)) {
       if (cookies) {
         document.cookie = `data=${val + cookies.split("data=")}`;
         cookies = document.cookie;
-        $("#ft_list").prepend(`<li id="list">${val}</li>`);
+        $("#ft_list").prepend(`<li id="list">${decodeURIComponent(val)}</li>`);
       } else {
         document.cookie = `data=${val}`;
         cookies = document.cookie;
-        $("#ft_list").prepend(`<li>${val}</li>`);
+        $("#ft_list").prepend(`<li>${decodeURIComponent(val)}</li>`);
       }
     }
   });
 
   $("#ft_list").on("click", "#list", function () {
     if (confirm("Do you want to delete this task?")) {
-      const text = $(this).text();
+      const text = encodeURIComponent($(this).text());
       let data = cookies.substring(5);
       let updatedData = data
         .split(",") // string to array
@@ -30,10 +29,12 @@ $(document).ready(function () {
   });
 
   function showList() {
-    if (list[0] !== "") {
-      for (let i in list) {
-        if (list[i] !== "")
-          $("#ft_list").append(`<li class="${i}" id="list">${list[i]}</li>`);
+    var list = decodeURIComponent(document.cookie.substring(5));
+    let words = list.split(",");
+    if (words[0] !== "") {
+      for (let i in words) {
+        if (words[i] !== "" && words[i] !== "null")
+          $("#ft_list").append(`<li class="${i}" id="list">${words[i]}</li>`);
       }
     }
   }
