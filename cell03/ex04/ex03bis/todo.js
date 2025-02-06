678a1e70e566ad33eaaceb5ed23a5d2f410983b9
patdanai767 <pattproc@gmail.com>
2025-02-06 14:50:09 +0700

    finish all

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,28 +1,41 @@
 $(document).ready(function () {
-  const list = $("#ft_list");
-
+  var cookies = document.cookie;
+  var list = document.cookie.substring(5).split(",");
   $(".btn-add").on("click", function () {
-    const val = prompt("What is your name?");
-    if ((val !== "") & (val !== null)) {
-      var li = $("<li>");
-      var x = $("<p>");
-      $(li).html("");
-      $(x).html(val);
-      list.prepend(li);
-      li.prepend(x);
-      document.cookie = `data=${$(list).prop("outerHTML")}`;
-    }
-  });
-
-  $(list).on("click", function (e) {
-    if (e.target.tagName === "P") {
-      const checked = confirm(`Are you sure to delete?`);
-      if (checked) {
-        e.target.parentElement.remove();
-        document.cookie = `data=${$(list).prop("outerHTML")}`;
+    const val = encodeURIComponent(prompt());
+    if ((val !== null) & (val !== "")) {
+      if (cookies) {
+        document.cookie = `data=${val + cookies.split("data=")}`;
+        cookies = document.cookie;
+        $("#ft_list").prepend(`<li id="list">${val}</li>`);
+      } else {
+        document.cookie = `data=${val}`;
+        cookies = document.cookie;
+        $("#ft_list").prepend(`<li>${val}</li>`);
       }
     }
   });
 
-  $(list).html(document.cookie.substring(5));
+  $("#ft_list").on("click", "#list", function () {
+    if (confirm("Do you want to delete this task?")) {
+      const text = $(this).text();
+      let data = cookies.substring(5);
+      let updatedData = data
+        .split(",") // string to array
+        .filter((item) => item !== text)
+        .join(","); //back to string
+      document.cookie = `data=${updatedData}`;
+      $(this).remove();
+    }
+  });
+
+  function showList() {
+    if (list[0] !== "") {
+      for (let i in list) {
+        if (list[i] !== "")
+          $("#ft_list").append(`<li class="${i}" id="list">${list[i]}</li>`);
+      }
+    }
+  }
+  showList();
 });
